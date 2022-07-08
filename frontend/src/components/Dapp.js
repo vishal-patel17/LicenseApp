b64afa020de6d65569712a03bb4c6481f67e00a1
Vishal Patel <vishalpatel@Vishals-MacBook-Pro.local>
2022-07-08 11:58:56 +0530

    UI changes

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -89,7 +89,10 @@ export class Dapp extends React.Component {
                 </div>
                 <div className="form-group">
                   <label>Public addresses </label> <br />
-                  <select name="PublicAddr" onChange={this.handlePublicAddrChange}>
+                  <select
+                    name="PublicAddr"
+                    onChange={this.handlePublicAddrChange}
+                  >
                     <option>0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc</option>
                     <option>0x70997970c51812dc3a010c7d01b50e0d17dc79c8</option>
                     <option>0x90f79bf6eb2c4f870365e785982e1f101e93b906</option>
--- scripts/README.md
+++ scripts/README.md
@@ -1,1 +1,8 @@
-#Deploy to Local Network
+# Deploy to Local Network
+
+This script is responsible for:
+- Getting signers address
+- Getting the License contract
+- Saving the compiled License files to the frontend dir: src/contracts
+
+
